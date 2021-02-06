f0aa5af3b4f183dd92675e3ca268b662b32f3d30
EF3178 <64409068+EF3178@users.noreply.github.com>
2021-02-06 14:59:28 +0100

    4th commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # google-docs-mustaches-test
 I could not figure out how to use google-docs-mustaches library, so I decided to do the same but using directly google docs API.
 This code will :
-1) Copy a google docs file to a destination folder in Google Drice
+1) Copy a google docs file to a destination folder in Google Drive
 2) Update the created file
 3) Export the created file to pdf
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {google} = require('googleapis');
 const keydocs = require('./Keys-docs.json');
+const fs = require('fs');
+
 const client = new google.auth.JWT(
   keydocs.client_email,
   null,
@@ -15,7 +17,11 @@ client.authorize(function(err,tokens){
     return
   } else {
     console.log('connected');
-    gdruncopy(client)
+   // const runAllAPIs = async function(){
+   // const copiedFile = await gdruncopy(client);
+    //const updatedFile = await gdrunupdate(client, copiedFile);
+    gdrunexport(client);
+    //}
   }
 
 });
@@ -42,8 +48,28 @@ let requests = [
     },
   },
 ];
+
+
+//Copy google doc
+async function gdruncopy(cl, newFileId){
+  const gdriveapi = await google.drive({version:'v3', auth: cl })
+
+  var copyTitle = "NewCP";
+  let newrequest = {
+    name: copyTitle,
+  };
+  let copyFile = await gdriveapi.files.copy({
+    fileId: newFileId,
+    resource: newrequest,
+  }, (err, driveResponse) => {
+    let documentCopyId = driveResponse.data.id;
+    console.log(documentCopyId);
+    return documentCopyId
+  });
+};
+
 // Update data in google doc
-async function gdrun(cl){
+async function gdrunupdate(cl){
   const gdapi = await google.docs({version:'v1', auth: cl })
   const opt ={
       documentId: '1a4wZnQA-g3TsNJEcTzo3zO3zPZWH8DRv1ZlxpGUem4Q',
@@ -56,24 +82,28 @@ let data = await gdapi.documents.batchUpdate(opt)
 console.log(data)
 }
 
-
-//Copy google doc
-async function gdruncopy(cl){
+//Export new file as pdf
+async function gdrunexport(cl){
   const gdriveapi = await google.drive({version:'v3', auth: cl })
+ 
+  var fileId = '1a4wZnQA-g3TsNJEcTzo3zO3zPZWH8DRv1ZlxpGUem4Q';
+  var dest = fs.createWriteStream('./resume.pdf');
+ 
+  const exportedFile = await gdriveapi.files.export({
+    fileId: fileId,
+    mimeType: 'application/pdf'
+    },
+    {responseType: 'stream'});
+    console.log(exportedFile);
 
-  var copyTitle = "NewCP";
-  let newrequest = {
-    name: copyTitle,
-  };
-  let copyFile = await gdriveapi.files.copy({
-    fileId: '1a4wZnQA-g3TsNJEcTzo3zO3zPZWH8DRv1ZlxpGUem4Q',
-    resource: newrequest,
-  }, (err, driveResponse) => {
-    let documentCopyId = driveResponse.data.id;
-    console.log(documentCopyId)
-  });
-
-
-}
+    await new Promise((resolve, reject) => {
+      exportedFile.data
+        .on('error', reject)
+        .pipe(dest)
+        .on('error', reject)
+        .on('finish', resolve);
+    });
+ 
+};
 
 
